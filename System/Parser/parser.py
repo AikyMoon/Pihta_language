fae4e64caefe4a1f7f584b0a3cecd0f7cbb845d3
Zaharic <88633850+Zaharic@users.noreply.github.com>
2021-11-23 20:23:53 +0300

    Update parser.py

--- System/Parser/parser.py
+++ System/Parser/parser.py
@@ -1,44 +1,18 @@
 import re
 
 class Parser(object):
-
-<<<<<<< HEAD
-<<<<<<< HEAD
     def __init__(self, mem, error_line):
-=======
-    def __init__(self, tokens, mem, error_line):
-        self.tokens = tokens
-        self.token_index = 0
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
-    def __init__(self, tokens, mem, error_line):
-        self.tokens = tokens
-        self.token_index = 0
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
         self.variables = {}
         self.mem = mem
         self.error_line = error_line
-
-<<<<<<< HEAD
-<<<<<<< HEAD
     def parse(self, tokens):
         self.token_index = 0
         self.tokens = tokens
-=======
-    def parse(self):
-
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
-    def parse(self):
-
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
         while self.token_index < len(self.tokens):
 
             type = self.tokens[self.token_index][0]
             value = self.tokens[self.token_index][1]
 
-<<<<<<< HEAD
-<<<<<<< HEAD
             if type == 'var_declare' and value == 'var' and self.mem.if_was == False:
                 self.var(self.tokens[self.token_index:])
             elif type == 'output' and value == 'console' and self.mem.if_was == False:
@@ -47,17 +21,6 @@ class Parser(object):
                 self.trigger(self.tokens[self.token_index:])
             elif self.mem.if_was == True:
                 self.trigger(self.tokens[self.token_index:])
-=======
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-            if type == 'var_declare' and value == 'var':
-                self.var(self.tokens[self.token_index:])
-            elif type == 'output' and value == 'console':
-                self.console(self.tokens[self.token_index + 1:self.token_index + 3])
-<<<<<<< HEAD
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
             self.token_index += 1
 
     def console(self, value):
@@ -75,13 +38,7 @@ class Parser(object):
                     quit()
             else:
                 print(value[0][1])
-<<<<<<< HEAD
-<<<<<<< HEAD
         self.token_index += 2
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
 
     
     def var(self, tokens_list):
@@ -102,34 +59,13 @@ class Parser(object):
         for j in range(3, len(lst)):
             if lst[j][0] == 'var_ident':
                 # var_value = self.variables[lst[j][1]]
-<<<<<<< HEAD
-<<<<<<< HEAD
                 var = re.search(r'[a-zA-Z]+', lst[j][1])[0]
                 var_val = self.mem.save_vars[var]
                 to_eval_string += lst[j][1].replace(var, str(var_val))
-=======
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-                if '(' in lst[j][1]:
-                    var_value = self.mem.save_vars[lst[j][1][1:]]
-                    to_eval_string += '(' + str(var_value)
-                elif ')' in lst[j][1]:
-                    var_value = self.mem.save_vars[lst[j][1][:-1]]
-                    to_eval_string += str(var_value) + ')'
-                else:
-                    var_value = self.mem.save_vars[lst[j][1]]
-                    to_eval_string += str(var_value)
-<<<<<<< HEAD
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
             elif lst[j][0] == 'input':
                 to_eval_string = input()
             else:
                 to_eval_string += str(lst[j][1])
-<<<<<<< HEAD
-<<<<<<< HEAD
-
         self.mem.save(lst[1][1], eval(to_eval_string))
         self.token_index += len(lst)
             # self.variables[lst[1][1]] = eval(to_eval_string)
@@ -165,21 +101,3 @@ class Parser(object):
                     # print('True')
                     # print(self.mem.true_condition)
                     self.parse(self.mem.true_condition)
-=======
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-        if '(' in lst[1][1] and ')' in lst[1][1]:
-            s = lst[1][1].find('(')
-            e = lst[1][1].find(')')
-            brackets_eval = eval(lst[1][1][s:e+1])
-            to_eval_string = to_eval_string[:s] + str(brackets_eval) + to_eval_string[e:]
-            self.mem.save(lst[1][1], eval(to_eval_string))
-        else:
-            # print(to_eval_string)
-            self.mem.save(lst[1][1], eval(to_eval_string))
-            # self.variables[lst[1][1]] = eval(to_eval_string)
-
-<<<<<<< HEAD
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
