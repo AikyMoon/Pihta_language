670e79e2f2c9f2e8cab07e577de170a6e5b69f5c
Zaharic <88633850+Zaharic@users.noreply.github.com>
2021-11-23 19:32:07 +0300

    Update lex.py

--- System/Lexer/lex.py
+++ System/Lexer/lex.py
@@ -1,8 +1,7 @@
 import re
 # создаем словарь для части токенов и  их регулярок
 tokens_dict = {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     'if' : r'trigger',
     'var_declare' : r'var',
     'output' : r'console',
@@ -14,20 +13,6 @@ tokens_dict = {
     'start_func' : r'{',
     'end_func' : r'}',
     'void_line' : r'\n'
-=======
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-    'var_declare' : r'var',
-    'output' : r'console',
-    'input' : r'entry',
-    'var_ident' : r'[a-zA-Z()]+',
-    # 'operator' : r'[+-=/*]',
-    'digit' : r'\d+',
-<<<<<<< HEAD
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-    # 'string' : r'"',
 }
 # класс лексера
 class Lexer(object):
@@ -37,20 +22,8 @@ class Lexer(object):
     # функйия для токенизации
     def tokenize(self, error):
         # создаем список слов
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         if self.content[-1] in '{};':
-=======
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-        if self.content[-1] != ';':
-            print(f'Отсутствует ; в конце строки {error}')
-            quit()
-        else:
-<<<<<<< HEAD
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
             words_to_tokenize = self.content.split()
             # стартовый индекс списка и сисок с токенами
             cur_index = 0
@@ -61,15 +34,8 @@ class Lexer(object):
                 word = words_to_tokenize[cur_index]
                 # цикл для перебора регулярок и определение токена и его значения
                 for r_comp in tokens_dict.keys():
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     if re.search(tokens_dict[r_comp], word):
-=======
-                    if re.match(tokens_dict[r_comp], word):
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
-                    if re.match(tokens_dict[r_comp], word):
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
                         if word[-1] == ';':
                             word = word[:-1]
                             tokens.append([r_comp, word])
@@ -86,8 +52,7 @@ class Lexer(object):
 
                 cur_index += 1
             # возвращаем список токенов
-<<<<<<< HEAD
-<<<<<<< HEAD
+
             return tokens
 
         elif self.content == None:
@@ -95,9 +60,5 @@ class Lexer(object):
         else:
             print(f'Отсутствует ; в конце строки {error}')
             quit()
-=======
             return tokens
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
-            return tokens
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
+
