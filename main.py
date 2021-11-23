dab985ce112641e03c19d6d3e869bfbc3380dded
Zaharic <88633850+Zaharic@users.noreply.github.com>
2021-11-23 19:27:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 from System.Compiler import *
 
 filename = input()
-<<<<<<< HEAD
-<<<<<<< HEAD
 Compiler(filename)
-=======
-Compiler(folder + filename)
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
-=======
-Compiler(folder + filename)
->>>>>>> 078f9820365c24e2c64eb8df907b0190d0309579
 
 input()
